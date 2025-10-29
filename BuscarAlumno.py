ec41f0b4e4c49a6edf1a3452aa5fb7520b3285d1
JCarlos <juan.ticlia@utec.edu.pe>
2025-10-29 08:46:54 -0500

    Add files via upload

--- BuscarAlumno.py
+++ BuscarAlumno.py
@@ -2,8 +2,9 @@ import boto3
 
 def lambda_handler(event, context):
     # Entrada (json)
-    tenant_id = event['tenant_id']
-    alumno_id = event['alumno_id']
+    print(event)
+    tenant_id = event['body']['tenant_id']
+    alumno_id = event['body']['alumno_id']
     # Proceso
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('t_alumnos')
